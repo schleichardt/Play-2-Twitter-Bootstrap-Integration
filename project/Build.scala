329a681b50f64a176f5d97b325d36b50bad4f821
Michael Schleichardt <michael@schleichardt.net>
2012-09-22 18:37:44 +0200

    setup publishing-configuration

--- project/Build.scala
+++ project/Build.scala
@@ -5,14 +5,17 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "Play-2-Twitter-Bootstrap-Integration"
-    val appVersion      = "1.0-SNAPSHOT"
+    val moduleOrganization = "info.schleichardt"
+    val appVersion      = "0.1-SNAPSHOT"
+    val publishingFolder = Path.userHome.absolutePath+"/Projekte/schleichardt.github.com/jvmrepo"
 
     val appDependencies = Seq(
       // Add your project dependencies here,
     )
 
     val main = PlayProject(appName, appVersion, appDependencies).settings(
-      // Add your own project settings here      
+      organization := moduleOrganization,
+      publishTo := Some(Resolver.file("file",  new File(publishingFolder))(Resolver.mavenStylePatterns)),
+      publishMavenStyle := true
     )
-
 }
